521aae655869e59a9b182ba2e09b3940a5f04418
YuXiaoC <1912850912@qq.com>
2020-05-12 19:56:43 +0800

    优化myfns.js文件深克隆方法

--- myfns.js
+++ myfns.js
@@ -11,14 +11,14 @@ function deepClone (origin,target) {
         tostr = Object.prototype.toString,
         arrStr = "[object Array]";
     for(var prop in origin){
-        // hasOwnProperty
         if(origin.hasOwnProperty(prop)){
-            if(typeof origin[prop] == "object"){  //判断属性是引用类型还是原始值
-                if(tostr.call(origin[prop]) == arrStr){  // 判断值为数组还是对象
-                    target[prop] = []
-                }else{
-                    target[prop] = {}
-                }
+            if(origin[prop] !== "null" && typeof origin[prop] == "object"){  //判断属性是引用类型还是原始值
+                // if(tostr.call(origin[prop]) == arrStr){  // 判断值为数组还是对象
+                //     target[prop] = []
+                // }else{
+                //     target[prop] = {}
+                // }
+                target[prop] = tostr.call(origin[prop]) == arrStr ? [] : {} // 使用三木运算符代替以上if的写法
                 deepClone(origin[prop],target[prop]) // 递归
             }else{
                 target[prop] = origin[prop]
